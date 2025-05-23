7a593db35f30b44b635191afb5f14b8f8ba8e2f8
RafliPrasetya <rafliprasetya07@gmail.com>
2025-05-24 06:19:15 +0700

    update tugas 5 show, update, destroy data

--- routes/api.php
+++ routes/api.php
@@ -10,20 +10,7 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-
-Route::get('/books', [BookController::class, 'index']);
-Route::get('/books/{id}', [BookController::class, 'show']);
-Route::delete('/books/{id}', [BookController::class, 'destroy']);
-Route::post('/books', [BookController::class, 'store']);
-
-Route::get('/genres', [GenreController::class, 'index']);
-Route::get('/genres/{id}', [GenreController::class, 'show']);
-Route::delete('/genres/{id}', [GenreController::class, 'destroy']);
-Route::post('/genres/{id}', [GenreController::class, 'update']);
-Route::post('/genres', [GenreController::class, 'store']);
-
-Route::get('/authors', [AuthorController::class, 'index']);
-Route::get('/authors/{id}', [AuthorController::class, 'show']);
-Route::delete('/authors/{id}', [AuthorController::class, 'destroy']);
-Route::post('/authors/{id}', [AuthorController::class, 'update']);
-Route::post('/authors', [AuthorController::class, 'store']);
+// apiResource untuk endpoint RESTful (index, show, store, update, destroy)
+Route::apiResource('books', BookController::class);
+Route::apiResource('genres', GenreController::class);
+Route::apiResource('authors', AuthorController::class);
